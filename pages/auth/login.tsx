efe20a0f57b3fc22b4f17a26bc78c2ae37ce2d20
Jihye <kimji9663@nate.com>
2022-07-07 18:50:56 +0900

    패스워드 유효성 검사

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form";
 import Button from "../../src/components/common/button";
 import LinkButton from "../../src/components/common/linkButton";
 import InputBox from "../../src/components/common/Input";
+import { InputLabel } from "../../src/components/common/Input/index.css"
 
 interface IFormInput {
   email: string;
@@ -27,7 +28,7 @@ export default function LoginPage() {
       <form onSubmit={handleSubmit(onSubmit)}>
         {/* <input {...register("email")} /> */}
         {/* <input {...register("password")} /> */}
-        <InputBox icon="email" type="text" 
+        {/* <InputBox icon="email" type="text" 
           {...register("email", {
             required: true,
           })} 
@@ -40,8 +41,46 @@ export default function LoginPage() {
             pattern: /^[A-Za-z]+$/i
           })} 
          />
-         {errors?.password?.type === 'pattern' && <p>영문/숫자/특수문자 2가지 이상 조합 (8~20자)</p>}
-         {/* InputBox를 공통으로 쓰는 것 때문에 문제가 생긴듯 register를 각 input의 props로 넘기는 방법 찾아야 함 */}
+         {errors?.password?.type === 'pattern' && <p>영문/숫자/특수문자 2가지 이상 조합 (8~20자)</p>} */}
+         
+        <InputLabel>
+          <span className="icon_label">
+            <i className={`sprite email`}></i>
+          </span>
+          <span className="validator">
+            <i className="sprite valid"></i>
+          </span>
+          <span className="input_wrap">
+            <input type="text" 
+              {...register("email", {
+                required: true,
+              })}
+            />
+          </span>
+        </InputLabel>
+        {errors?.email?.type === 'required' && <p>이메일을 입력하세요.</p>}
+
+        <InputLabel>
+          <span className="icon_label">
+            <i className={`sprite password`}></i>
+          </span>
+          <span className="validator">
+            <i className="sprite valid"></i>
+          </span>
+          <span className="input_wrap">
+            <input type="password" 
+              {...register("password", {
+                required: true,
+                maxLength: 20,
+                pattern: /^(?=.*[a-z])((?=.*\d)|(?=.*\W))(?=.*[!@#$%^*+=-]).{8,20}$/i,
+              })} 
+            />
+          </span>
+        </InputLabel>
+        {errors?.password?.type === 'pattern' && <p>영문/숫자/특수문자 2가지 이상 조합 (8~20자)</p>}
+        {/* {errors?.password?.type === 'pattern' && <p>3개 이상 연속되거나 동일한 문자/숫자 제외</p>} */}
+        {/* {errors?.password?.type === 'pattern' && <p>아이디(이메일) 제외</p>} */}
+
         <InputBox icon="name" type="text" />
         <InputBox icon="phone" type="text" />
         <Button type="submit" variant="contained" color="primary" title="로그인" />
